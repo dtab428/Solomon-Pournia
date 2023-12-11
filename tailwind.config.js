35e56638cc70cd81c282483eabdf8736052c9bd2
Dan <dtab428@gmail.com>
2023-12-11 20:59:36 +0000

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,23 @@ import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Poppins", "sans-serif"], // Replace 'YourFontName',
-			},
-		},
-	},
-	darkMode: "class",
-	plugins: [nextui()],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // Replace 'YourFontName',
+      },
+      colors: {
+        "custom-yellow": "rgb(255 215 0)",
+        "custom-blue": "rgb(0 0 255)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
 };
