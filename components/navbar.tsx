481c82d0ddcdf0c39c9449beaf6ac8edb7dd4533
Dan <dtab428@gmail.com>
2023-12-14 22:44:36 -0800

    k

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -30,141 +30,35 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-	// const searchInput = (
-	// 	<Input
-	// 		aria-label="Search"
-	// 		classNames={{
-	// 			inputWrapper: "bg-default-100",
-	// 			input: "text-sm",
-	// 		}}
-	// 		endContent={
-	// 			<Kbd className="hidden lg:inline-block" keys={["command"]}>
-	// 				K
-	// 			</Kbd>
-	// 		}
-	// 		labelPlacement="outside"
-	// 		placeholder="Search..."
-	// 		startContent={
-	// 			<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-	// 		}
-	// 		type="search"
-	// 	/>
-	// );
-
 	return (
 		<NextUINavbar
 			maxWidth="sm"
 			position="sticky"
-			className="inline-block translate-x-[-50%] left-[50%] w-auto rounded-3xl top-2"
+			className="bg-transparent flex-wrap before:content-[''] before:block before:h-[0.5rem] before:w-full"
+			shouldHideOnScroll
+			isBlurred={false}
 		>
-			<div className="flex w-full items-center">
-				<NavbarContent className="basis-1/5 sm:basis-full" justify="center">
+			<div className="flex w-full items-center inline-block w-auto rounded-3xl bg-white backdrop-blur-lg bg-teal-200/25 p-3">
+				<NavbarContent className="" justify="center">
 					<NavbarBrand as="li" className="gap-3 max-w-fit">
 						<NextLink
 							className="flex justify-center items-center gap-1"
 							href="/"
 						>
 							{/* <Logo /> */}
-							<p className="font-bold text-inherit lowercase text-3xl dahlia-font">
+							<p className="font-bold text-black dark:text-white lowercase text-3xl dahlia-font">
 								Solomon Pournia
 							</p>
 						</NextLink>
 					</NavbarBrand>
-					{/* <ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({
-										color: "foreground",
-									}),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
-					))}
-				</ul> */}
 				</NavbarContent>
 
-				<NavbarContent
-					className="hidden sm:flex basis-1/5 sm:basis-full"
-					justify="end"
-				>
-					<NavbarItem className="hidden sm:flex gap-2">
-						{/* <Link
-                              isExternal
-                              href={siteConfig.links.twitter}
-                              aria-label="Twitter"
-                         >
-                              <TwitterIcon className="text-default-500" />
-                         </Link>
-                         <Link
-                              isExternal
-                              href={siteConfig.links.discord}
-                              aria-label="Discord"
-                         >
-                              <DiscordIcon className="text-default-500" />
-                         </Link>
-                         <Link
-                              isExternal
-                              href={siteConfig.links.github}
-                              aria-label="Github"
-                         >
-                              <GithubIcon className="text-default-500" />
-                         </Link> */}
+				<NavbarContent className="" justify="end">
+					<NavbarItem className="flex ms-2 gap-2">
 						<ThemeSwitch />
 					</NavbarItem>
-					{/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
-					<NavbarItem className="hidden md:flex">
-						{/* <Button
-                              isExternal
-                              as={Link}
-                              className="text-sm font-normal text-default-600 bg-default-100"
-                              href={siteConfig.links.sponsor}
-                              startContent={
-                                   <HeartFilledIcon className="text-danger" />
-                              }
-                              variant="flat"
-                         >
-                              Sponsor
-                         </Button> */}
-					</NavbarItem>
-				</NavbarContent>
-
-				<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-					<Link isExternal href={siteConfig.links.github} aria-label="Github">
-						{/* <GithubIcon className="text-default-500" /> */}
-					</Link>
-					<ThemeSwitch />
-					{/* <NavbarMenuToggle /> */}
 				</NavbarContent>
 			</div>
-			{/* <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu> */}
 		</NextUINavbar>
 	);
 };
