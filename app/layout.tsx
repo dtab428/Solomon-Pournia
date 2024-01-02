5ba1e91fca834cdbaa56f2b5a5390c62057a1e7a
Dan <dtab428@gmail.com>
2024-01-02 22:46:47 +0000

    new styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,70 +8,70 @@ import { Link } from "@nextui-org/react";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	// themeColor: [
-	//      { media: "(prefers-color-scheme: light)", color: "white" },
-	//      { media: "(prefers-color-scheme: dark)", color: "black" },
-	// ],
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  // themeColor: [
+  //      { media: "(prefers-color-scheme: light)", color: "white" },
+  //      { media: "(prefers-color-scheme: dark)", color: "black" },
+  // ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head>
-				<meta
-					name="theme-color"
-					content="white"
-					media="(prefers-color-scheme: light)"
-				/>
-				<meta
-					name="theme-color"
-					content="black"
-					media="(prefers-color-scheme: dark)"
-				/>
-			</head>
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
-			>
-				<Providers
-					themeProps={{
-						attribute: "class",
-						defaultTheme: "dark",
-					}}
-				>
-					<Navbar />
-					<main>{children}</main>
-					<footer className="w-full flex items-center justify-center pt-3 pb-6">
-						<Link
-							isExternal
-							className="flex items-center gap-1 text-current text-lg"
-							href="https://www.dtab.xyz"
-							title="Daniel Tabibzadeh's Portfolio"
-						>
-							<span className="text-default-600">
-								Built with <span className="text-red-500">♥</span> by
-							</span>
-							<span className="text-blue-500">dtab.xyz</span>
-						</Link>
-					</footer>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          name="theme-color"
+          content="white"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="black"
+          media="(prefers-color-scheme: dark)"
+        />
+      </head>
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased bg-gray-100/70",
+          fontSans.variable
+        )}
+      >
+        <Providers
+          themeProps={{
+            attribute: "class",
+            defaultTheme: "dark",
+          }}
+        >
+          <Navbar />
+          <main>{children}</main>
+          <footer className="w-full flex items-center justify-center pt-3 pb-6">
+            <Link
+              isExternal
+              className="flex items-center gap-1 text-current text-lg"
+              href="https://www.dtab.xyz"
+              title="Daniel Tabibzadeh's Portfolio"
+            >
+              <span className="text-default-600">
+                Built with <span className="text-red-500">♥</span> by
+              </span>
+              <span className="text-blue-500">dtab.xyz</span>
+            </Link>
+          </footer>
+        </Providers>
+      </body>
+    </html>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,11 @@
 import {
-	Navbar as NextUINavbar,
-	NavbarContent,
-	NavbarMenu,
-	NavbarMenuToggle,
-	NavbarBrand,
-	NavbarItem,
-	NavbarMenuItem,
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/react";
 import { Kbd } from "@nextui-org/react";
@@ -20,44 +20,44 @@ import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-	TwitterIcon,
-	GithubIcon,
-	DiscordIcon,
-	HeartFilledIcon,
-	SearchIcon,
+  TwitterIcon,
+  GithubIcon,
+  DiscordIcon,
+  HeartFilledIcon,
+  SearchIcon,
 } from "@/components/icons";
 
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-	return (
-		<NextUINavbar
-			maxWidth="sm"
-			position="static"
-			className="bg-transparent flex-wrap before:content-[''] before:block before:h-[0.5rem] before:w-full"
-			isBlurred={false}
-		>
-			<div className="flex w-full items-center inline-block w-auto rounded-3xl backdrop-blur-lg bg-teal-200/25 p-3">
-				<NavbarContent className="" justify="center">
-					<NavbarBrand as="li" className="gap-3 max-w-fit">
-						<NextLink
-							className="flex justify-center items-center gap-1"
-							href="/"
-						>
-							{/* <Logo /> */}
-							<p className="font-bold text-black dark:text-white lowercase text-5xl dahlia-font">
-								Solomon Pournia
-							</p>
-						</NextLink>
-					</NavbarBrand>
-				</NavbarContent>
+  return (
+    <NextUINavbar
+      maxWidth="sm"
+      position="static"
+      className="bg-white flex-wrap before:content-[''] before:block before:h-[0.5rem] before:w-full"
+      isBlurred={false}
+    >
+      <div className="flex w-full items-center inline-block w-auto rounded-3xl backdrop-blur-lg bg-teal-200/25 p-3">
+        <NavbarContent className="" justify="center">
+          <NavbarBrand as="li" className="gap-3 max-w-fit">
+            <NextLink
+              className="flex justify-center items-center gap-1"
+              href="/"
+            >
+              {/* <Logo /> */}
+              <p className="font-bold text-black dark:text-white lowercase text-5xl dahlia-font">
+                Solomon Pournia
+              </p>
+            </NextLink>
+          </NavbarBrand>
+        </NavbarContent>
 
-				<NavbarContent className="" justify="end">
-					<NavbarItem className="flex ms-2 gap-2">
-						<ThemeSwitch />
-					</NavbarItem>
-				</NavbarContent>
-			</div>
-		</NextUINavbar>
-	);
+        <NavbarContent className="" justify="end">
+          <NavbarItem className="flex ms-2 gap-2">
+            <ThemeSwitch />
+          </NavbarItem>
+        </NavbarContent>
+      </div>
+    </NextUINavbar>
+  );
 };
